294e886524727b598fa3667503d013f663a850d3
Даниил <danilka.volkov.02@mail.ru>
2026-04-30 22:02:17 +0300

    Add AppIcon

---
+++ frontend/src/shared/ui/app-icon/AppIcon.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+  interface Props {
+    name: string;
+    width: string;
+    height: string;
+  }
+
+  defineProps<Props>();
+</script>
+
+<template>
+  <svg
+    class="icon"
+    :width="width"
+    :height="height"
+  >
+    <use :href="`/sprite.svg#${name}`" />
+  </svg>
+</template>
