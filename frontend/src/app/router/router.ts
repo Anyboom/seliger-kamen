bd752709da01aeff2beb2d0bbeca90e319a60c74
Даниил Волков <dvolkov@corp.local>
2026-04-30 13:07:17 +0300

    Change dir page for FSD

--- frontend/src/app/router/router.ts
+++ frontend/src/app/router/router.ts
@@ -1,8 +1,8 @@
-import MainView from "@/pages/MainView.vue";
 import { createMemoryHistory, createRouter } from "vue-router";
 import { RouteNames } from "@/app/router/route-names.ts";
 import { Layouts } from "@/app/layouts/layouts.ts";
 import { loadLayout } from "@/app/router/middleware/load-layout.ts";
+import { MainView } from "@/pages/main";
 
 const routes = [
   {
