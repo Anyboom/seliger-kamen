8b651a334d26e69339b89341586ed4368050761d
Даниил <danilka.volkov.02@mail.ru>
2026-05-02 03:10:27 +0300

    Add AppFooter

--- frontend/src/pages/main/ui/MainView.vue
+++ frontend/src/pages/main/ui/MainView.vue
@@ -3,12 +3,14 @@
   import { AboutUs } from "@/widgets/about-us";
   import { OurAdvantages } from "@/widgets/our-advantages";
   import { OftenAsk } from "@/widgets/often-ask";
+  import { AppFooter } from "@/widgets/app-footer";
 </script>
 <template>
   <MainBanner />
   <AboutUs />
   <OurAdvantages />
   <OftenAsk />
+  <AppFooter />
 </template>
 
 <style scoped></style>
---
+++ frontend/src/widgets/app-footer/ui/AppFooter.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+  import { AppIcon } from "@/shared/ui/app-icon";
+</script>
+
+<template>
+  <div class="app-footer">
+    <div class="app-footer__wrapper">
+      <a
+        href="/"
+        class="app-footer__logo"
+      >
+        <AppIcon
+          name="logo"
+          width="176px"
+          height="46px"
+        />
+      </a>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  @use "@/shared/assets/styles/semantic";
+  @use "@/shared/assets/styles/mixins";
+  @use "@/shared/assets/styles/core";
+
+  .app-footer {
+    @include mixins.container;
+
+    &__wrapper {
+      padding: core.$spacing-400;
+      border-radius: core.$radius-200;
+      background: semantic.$background-default-negative;
+    }
+
+    &__logo {
+      color: semantic.$text-default-negative;
+      transition: color core.$transition-duration core.$transition-timing;
+
+      @include mixins.hover {
+        color: semantic.$text-default-negative-hover;
+      }
+    }
+  }
+</style>
