bbc3e42ecf6b8f9ec99248e338c976c1ccdf7c2e
Даниил Волков <dvolkov@corp.local>
2026-05-05 10:25:10 +0300

    Add env

--- frontend/src/app/router/middleware/load-page.ts
+++ frontend/src/app/router/middleware/load-page.ts
@@ -3,7 +3,7 @@ import type { RouteLocationNormalized } from "vue-router";
 
 export async function loadPage(route: RouteLocationNormalized): Promise<void> {
   try {
-    route.meta.currentPage = await ofetch(`https://directus.localhost/resolve-route${route.path}`);
+    route.meta.currentPage = await ofetch(`${import.meta.env.VITE_DIRECTUS}/resolve-route${route.path}`);
   } catch (error) {
     console.error("Не получилось выполнить запрос:", error);
   }
